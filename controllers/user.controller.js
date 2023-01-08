e19e7c89e8b4bf61963b76cc49d484463b383317
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-01-08 09:46:46 +0300

    Feat: project setups

---
+++ controllers/user.controller.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route was not implemented yet.',
+  });
+};
+
+exports.getSpecificUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route was not implemented yet.',
+  });
+};
+
+exports.addNewUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route was not implemented yet.',
+  });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route was not implemented yet.',
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route was not implemented yet.',
+  });
+};
