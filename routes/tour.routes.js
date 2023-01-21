59c9f906ba6fc7a8cfa7acf621566dc2e05e3267
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-01-21 11:39:55 +0300

    feat: statistics tours, tours plans monthly routes

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -6,12 +6,17 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tour.controllers');
 
 const router = express.Router();
 
 // Aliasing
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plans/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(AddNewTour);
 router.route('/:id').get(getSpecificTour).patch(updateTour).delete(deleteTour);
