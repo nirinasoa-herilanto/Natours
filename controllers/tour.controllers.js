d68103ad5014e0594ded06de43debb20b22dc7fd
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-01-21 11:38:23 +0300

    feat: aggregation pipeline on tours
    Tours statistics & Tours plans monthly

--- controllers/tour.controllers.js
+++ controllers/tour.controllers.js
@@ -9,22 +9,6 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.AddNewTour = async (req, res) => {
-  try {
-    const newTour = await Tour.create(req.body);
-
-    res.status(201).json({
-      success: 'success',
-      data: { tour: newTour },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: 'Invalid data input!',
-    });
-  }
-};
-
 exports.getAllTours = async (req, res) => {
   try {
     const features = new APIFeatures(Tour, req.query)
@@ -101,6 +85,104 @@ exports.deleteTour = async (req, res) => {
     res.status(404).json({
       status: 'fail',
       message: 'Resource not found!',
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          // "null" means all Tour, or we can set a specific fields by adding $fields
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRating: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Resource not found!',
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1; // 2021
+
+    console.log(year);
+
+    const plans = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStart: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: {
+          month: '$_id',
+        },
+      },
+      {
+        $project: {
+          _id: 0, // we can manipulate by adding 0/1 (showing this value)
+          // numTourStart: 1,
+        },
+      },
+      {
+        $sort: {
+          month: 1,
+        },
+      },
+      // {
+      //   $limit: 5,
+      // },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { plans },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Ressource not found!',
     });
   }
 };
