c11769876a5698efd0c8ac3edc62a474c4a1f8a6
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-02-02 19:58:18 +0300

    update: add security middleware to prevent some attacks

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
 const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
 const AppError = require('./utils/appError.utils');
@@ -10,12 +15,39 @@ const globalErrorHandler = require('./controllers/error.controller');
  */
 const app = express();
 
+app.use(helmet()); // set security HTTP headers
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// ----- Middlewares -----
-app.use(express.json());
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  message: 'Too many request from this IP. Please try again after an hour!',
+});
+
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '1mb' })); // body parser, reading data from req.body
+
+app.use(mongoSanitize()); // to prevent for NoSQL injections
+app.use(xss());
+
+//to prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'price',
+      'difficulty',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+    ],
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
@@ -29,11 +61,6 @@ app.use('/api/v1/users', userRouter);
 
 // # Handling unhandled routes
 app.all('*', (req, res, next) => {
-  // # built error, same as AppError classes
-  // const err = new Error(`Can't find ${req.originalUrl} on the server.`);
-  // err.statusCode = 404;
-  // err.status = 'fail';
-
   const err = new AppError(`Can't find ${req.originalUrl} on the server.`, 404);
 
   next(err);
