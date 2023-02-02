9b493165766adeb1a542e94cf38689c20e90bca1
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-02-02 19:56:01 +0300

    feat: add user authentication routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const router = express.Router();
+
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  protect,
+} = require('../controllers/auth.controller');
+
 const {
   getAllUsers,
   getSpecificUser,
   addNewUser,
   updateUser,
   deleteUser,
+  updateProfile,
+  deleteAccount,
 } = require('../controllers/user.controller');
 
-const router = express.Router();
+router.post('/signup', signup);
+router.post('/login', login);
+
+router.post('/forgot-password', forgotPassword);
+router.patch('/reset-password/:token', resetPassword);
+router.patch('/update-password', protect, updatePassword);
+
+router.patch('/update-profile', protect, updateProfile);
+router.delete('/delete-account', protect, deleteAccount);
 
 router.route('/').get(getAllUsers).post(addNewUser);
 router.route('/:id').get(getSpecificUser).patch(updateUser).delete(deleteUser);
