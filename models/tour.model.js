2fe309fbf2e8afc6f85db3a68941d8d4466166d0
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-02-06 11:37:21 +0300

    update: modelling tours with child referencing

--- models/tour.model.js
+++ models/tour.model.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('./user.model');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -100,7 +99,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -115,14 +119,6 @@ tourSchema.virtual('durationWeeks').get(function () {
 // Document middleware: runs before ".save()" or ".create()"
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
-  next();
-});
-
-tourSchema.pre('save', async function (next) {
-  const guides = this.guides.map(async (id) => await User.findById(id).exec());
-
-  this.guides = await Promise.all(guides);
-
   next();
 });
 
