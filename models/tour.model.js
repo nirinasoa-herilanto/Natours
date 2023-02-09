2de58cbaed608a7a1496e5ac5d625048a650493b
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-02-09 09:27:52 +0300

    update: add geo index on startLocation

--- models/tour.model.js
+++ models/tour.model.js
@@ -33,6 +33,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be bellow 5.0'],
+      set: (val) => Math.round(val * 10) / 10, // (4.6666 * 10) / 10
     },
     ratingsQuantity: {
       type: Number,
@@ -111,6 +112,10 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+// create index on price, for purpose to improve perfomance on query
+tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
+
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
@@ -153,11 +158,11 @@ tourSchema.post(/^find/, function (doc, next) {
 });
 
 // Aggregation middleware
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 
-  next();
-});
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
