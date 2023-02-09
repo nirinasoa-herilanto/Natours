ebe55fdd8db85a0688bba0268a434f768164c272
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-02-09 09:25:21 +0300

    feat: add geospatial queries & aggregation

--- controllers/tour.controllers.js
+++ controllers/tour.controllers.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tour.model');
+const AppError = require('../utils/appError.utils');
 const catchAsync = require('../utils/catchAsync.util');
 const factory = require('./handle.factory');
 
@@ -15,6 +16,83 @@ exports.getSpecificTour = factory.getOne(Tour, { path: 'reviews' });
 exports.AddNewTour = factory.createOne(Tour);
 exports.updateTour = factory.updateOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
+
+/**
+ * - Geospatial queries: use to find tours availble with certain postion.
+ * - routes: '/tours-within/:distance/center/:coords/unit/:unit'
+ */
+exports.getTourWithin = catchAsync(async (req, res, next) => {
+  const { distance, coords, unit } = req.params;
+  const [lat, lng] = coords.split(',');
+
+  const radius = unit === 'ml' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        "Please provide your coordinates with this format 'lat,lng'",
+        400
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+/**
+ * Geospatial aggregation: use to get distance between certains position
+ */
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { coords, unit } = req.params;
+  const [lat, lng] = coords.split(',');
+
+  const multilier = unit === 'ml' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    return next(
+      new AppError(
+        "Please provide your coordinates with this format 'lat,lng'",
+        400
+      )
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multilier,
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        distance: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
+});
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
