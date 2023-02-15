98f37f2f3798e5bd26325aa32d0103f1d67e2263
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-02-15 16:37:41 +0300

    update: security headers

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const rateLimit = require('express-rate-limit');
+
 const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
 const reviewRouter = require('./routes/review.routes');
+const viewRouter = require('./routes/view.routes');
+
 const AppError = require('./utils/appError.utils');
 const globalErrorHandler = require('./controllers/error.controller');
 
@@ -16,12 +21,43 @@ const globalErrorHandler = require('./controllers/error.controller');
  * Natours application
  */
 const app = express();
+
+app.use(cors());
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(helmet()); // set security HTTP headers
+// app.use(helmet()); // set security HTTP headers
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'worker-src': ['blob:'],
+        'child-src': ['blob:'],
+        'img-src': ["'self'", 'data: image/webp'],
+        'script-src': [
+          "'self'",
+          'https://api.mapbox.com',
+          'https://cdnjs.cloudflare.com',
+          "'unsafe-inline'",
+        ],
+        'connect-src': [
+          "'self'",
+          'ws://localhost:*',
+          'ws://127.0.0.1:*',
+          'http://127.0.0.1:*',
+          'http://localhost:*',
+          'https://*.tiles.mapbox.com',
+          'https://api.mapbox.com',
+          'https://events.mapbox.com',
+        ],
+      },
+    },
+    crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: 'cross-origin',
+  })
+);
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -36,6 +72,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(express.json({ limit: '1mb' })); // body parser, reading data from req.body
+app.use(cookieParser());
 
 app.use(mongoSanitize()); // to prevent for NoSQL injections
 app.use(xss());
@@ -54,18 +91,13 @@ app.use(
   })
 );
 
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 // ----- Routes -----
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
