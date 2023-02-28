7b374565a02f55963bf6099f3bc8ff72bab5f3e4
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-02-28 14:07:40 +0300

    update: implementation booking on view

--- routes/view.routes.js
+++ routes/view.routes.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const { isLoggedIn, protect } = require('../controllers/auth.controller');
+const { createBookingCheckout } = require('../controllers/booking.controller');
 const {
   displayOveview,
   displayTourDetail,
   displayUserLogin,
   displayProfile,
+  viewMyTours,
 } = require('../controllers/view.controller');
 
 const router = express.Router();
 
 router.get('/login', isLoggedIn, displayUserLogin);
-router.get('/', isLoggedIn, displayOveview);
+router.get('/', createBookingCheckout, isLoggedIn, displayOveview);
 router.get('/tours/:slug', isLoggedIn, displayTourDetail);
 
 router.get('/profile', protect, displayProfile);
+router.get('/my-tours', protect, viewMyTours);
 
 module.exports = router;
