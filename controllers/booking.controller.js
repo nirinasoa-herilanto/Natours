6da02d606555f1e2f4adbe580b85cb52eb435f68
nirinasoa-herilanto <nirinasoa.herilanto@gmail.com>
2023-02-28 14:07:18 +0300

    feat: create booking tour functionnality

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -3,16 +3,18 @@ const { stripeApiKey } = require('../config');
 const stripe = require('stripe')(stripeApiKey);
 const Tour = require('../models/tour.model');
 const AppError = require('../utils/appError.utils');
+const Booking = require('../models/booking.model');
+const factory = require('../controllers/handle.factory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get the currently booked tour
+  // Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId).exec();
   // console.log(tour);
 
-  // 2) Create checkout session
+  // Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
       req.params.tourId
     }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tours/${tour.slug}`,
@@ -36,9 +38,31 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     ],
   });
 
-  // 3) Create session as response
+  // Create session as response
   res.status(200).json({
     status: 'success',
     session,
   });
 });
+
+/**
+ * Temporary for create booking checkout (unsecure)
+ * - in production, use stripe webhooks
+ */
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { user, tour, price } = req.query;
+
+  console.log(req.query);
+
+  if (!user && !tour && !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+});
+
+exports.getAllBookings = factory.getAll(Booking);
+exports.getSpecificBooking = factory.getOne(Booking);
+exports.addNewBooking = factory.createOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const { getCheckoutSession } = require('../controllers/booking.controller');
-const { protect } = require('../controllers/auth.controller');
+const {
+  getCheckoutSession,
+  getAllBookings,
+  addNewBooking,
+  updateBooking,
+  deleteBooking,
+} = require('../controllers/booking.controller');
+const { protect, restrictTo } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.get('/checkout-session/:tourId', protect, getCheckoutSession);
+router.use(protect);
+router.get('/checkout-session/:tourId', getCheckoutSession);
+
+router.use(restrictTo('lead-guide', 'admin'));
+router.route('/').get(getAllBookings).post(addNewBooking);
+router.route('/:id').patch(updateBooking).delete(deleteBooking);
 
 module.exports = router;
